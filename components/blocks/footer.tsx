8acbc14342883ebfb0a62bebb0842aa9014db29b
Boris Fedotov <bfedotov@gmail.com>
2025-07-15 08:01:35 +0200

    footer

--- components/blocks/footer.tsx
+++ components/blocks/footer.tsx
@@ -1,5 +1,5 @@
 import { Globe } from "lucide-react"
-import { File } from "lucide-react"
+// import { File } from "lucide-react"
 import Link from "next/link"
 
 export default function Footer() {
@@ -7,17 +7,17 @@ export default function Footer() {
     <footer className="row-start-4 flex flex-row gap-6 flex-wrap items-center justify-center bg-gray-900/70 py-2 px-4 rounded-xl mt-6">
       <Link
         className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-white"
-        href="/PrivacyPolicy.pdf"
+        href="https://avica.cloud/privacy"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <File />
+        <Globe />
         Privacy policy
       </Link>
 
       <Link
         className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-white"
-        href="https://www.seendrop.com/"
+        href="https://avica.cloud/"
         target="_blank"
         rel="noopener noreferrer"
       >
