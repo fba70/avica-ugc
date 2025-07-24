808e8a67f5e3668191f9951a7c1c33c2b5e6108c
Boris Fedotov <bfedotov@gmail.com>
2025-07-24 11:39:01 +0200

    image and video generation routes rate limiting

--- app/api/video-gen/route.js
+++ app/api/video-gen/route.js
@@ -1,13 +1,42 @@
 import { NextResponse } from "next/server"
 import Replicate from "replicate"
 
+import arcjet, { tokenBucket } from "@arcjet/next"
+import { auth } from "@clerk/nextjs/server"
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
+})
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY, // Get your site key from https://app.arcjet.com
+  characteristics: ["userId"], // track requests by a custom user ID
+  rules: [
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      refillRate: 1, // refill 1 token per interval
+      interval: 30, // refill every 30 seconds
+      capacity: 3, // bucket maximum capacity of 10 tokens
+    }),
+  ],
 })
 
 export async function POST(req) {
   if (!process.env.REPLICATE_API_TOKEN) {
     throw new Error("The REPLICATE_API_TOKEN environment variable is not set")
+  }
+
+  const { userId } = await auth()
+
+  const decision = await aj.protect(req, { userId, requested: 1 })
+  console.log("Arcjet decision", decision)
+
+  if (decision.isDenied()) {
+    return NextResponse.json(
+      { error: "Too Many Requests", reason: decision.reason },
+      { status: 429 }
+    )
   }
 
   const { ugcUrl, prompt } = await req.json()
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,46 @@
+import arcjet, { shield } from "@arcjet/next"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+export const config = {
+  // See https://clerk.com/docs/references/nextjs/clerk-middleware
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+}
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    // Protect against common attacks with Arcjet Shield
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+})
+
+const isProtectedRoute = createRouteMatcher(["/api/private"])
+
+// Arcjet runs first to protect all routes defined in the matcher config above.
+// Then if the request is allowed, Clerk runs
+export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req)
+
+  if (decision.isDenied()) {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 })
+  }
+
+  if (isProtectedRoute(req)) {
+    await auth.protect()
+  }
+
+  return NextResponse.next()
+})
+
+/*
 import { clerkMiddleware } from "@clerk/nextjs/server"
 
 export default clerkMiddleware()
@@ -10,3 +53,4 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 }
+*/
