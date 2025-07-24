31a0b95e9e68b189797dd4b3fb7bde60da32c473
Boris Fedotov <bfedotov@gmail.com>
2025-07-24 12:33:28 +0200

    ibot config

---
+++ instrumentation-client.ts
@@ -0,0 +1,41 @@
+import { initBotId } from "botid/client/core"
+
+// Define the paths that need bot protection:
+// - API endpoints (e.g., '/api/checkout')
+// - Server actions invoked from a page (e.g., '/dashboard')
+// - Dynamic routes (e.g., '/api/create/*')
+// Wildcards can be used to expand multiple segments
+// /team/*/activate will match
+// /team/a/activate
+// /team/a/b/activate
+// /team/a/b/c/activate
+// ...
+
+initBotId({
+  protect: [
+    {
+      path: "/api/events",
+      method: "POST",
+    },
+    {
+      path: "/api/image-gen-2",
+      method: "POST",
+    },
+    {
+      path: "/api/video-gen",
+      method: "POST",
+    },
+    {
+      path: "/api/user",
+      method: "POST",
+    },
+    {
+      path: "/api/seendrops",
+      method: "POST",
+    },
+    {
+      path: "/api/seendrops/*",
+      method: "POST",
+    },
+  ],
+})
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next"
+import { withBotId } from "botid/next/config"
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -45,4 +46,5 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withBotId(nextConfig)
+// export default nextConfig
