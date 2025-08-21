41a98ca313c944e2caba2059b0f9a1764be0face
Boris Fedotov <bfedotov@gmail.com>
2025-08-21 20:08:42 +0200

    build errors fixing

--- app/(protected)/payment/canceled/page.tsx
+++ app/(protected)/payment/canceled/page.tsx
@@ -1,28 +1,10 @@
-"use client"
+import { Suspense } from "react"
+import { PaymentCanceled } from "@/components/blocks/payment-canceled"
 
-import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
-import axios from "axios"
-
-export default function PaymentSuccess() {
-  const searchParams = useSearchParams()
-  const sessionId = searchParams.get("session_id")
-  const [orderId, setOrderId] = useState<string | null>(null)
-
-  useEffect(() => {
-    if (!sessionId) return
-    axios.get(`/api/stripe/session?session_id=${sessionId}`).then((res) => {
-      setOrderId(res.data.session.metadata.orderId)
-    })
-  }, [sessionId])
-
+export default function PaymentCanceledPage() {
   return (
-    <section className="w-full max-w-7xl flex flex-col items-center justify-center">
-      <div className="w-[360px]">
-        <h1>Your payment was not successful!</h1>
-        <h2>Product is not purchased!</h2>
-        {orderId && <p>Your purchase order ID: {orderId}</p>}
-      </div>
-    </section>
+    <Suspense>
+      <PaymentCanceled />
+    </Suspense>
   )
 }
